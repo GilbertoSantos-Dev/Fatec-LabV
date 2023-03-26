3920613d79584d6482f99b56ffdd9adb7db57e26
Gilberto Santos <gilbertosantosdev@gmail.com>
2023-03-25 21:24:11 -0300

    Tentando corrigir o teste

--- src/test/java/br/gov/sp/fatec/projetolab5/service/AutorizacaoServiceTest.java
+++ src/test/java/br/gov/sp/fatec/projetolab5/service/AutorizacaoServiceTest.java
@@ -8,10 +8,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-
 import br.gov.sp.fatec.projetolab5.repository.AutorizacaoRepository;
 
+@SpringBootTest
 public class AutorizacaoServiceTest {
 
     @Autowired
